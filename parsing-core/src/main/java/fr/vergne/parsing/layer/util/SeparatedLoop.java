cc3405df6c05ee0605193e76c30c51b0c651d1b0
Matthieu Vergne <matthieu.vergne@gmail.com>
2014-05-07 19:19:43 +0200

    Add a loop able to manage separators.

---
+++ parsing-core/src/main/java/fr/vergne/parsing/layer/util/SeparatedLoop.java
@@ -0,0 +1,110 @@
+package fr.vergne.parsing.layer.util;
+
+import fr.vergne.parsing.layer.Layer;
+import fr.vergne.parsing.layer.exception.ParsingException;
+import fr.vergne.parsing.layer.standard.AbstractLayer;
+import fr.vergne.parsing.layer.standard.Choice;
+import fr.vergne.parsing.layer.standard.GreedyMode;
+import fr.vergne.parsing.layer.standard.Loop;
+import fr.vergne.parsing.layer.standard.Loop.Generator;
+import fr.vergne.parsing.layer.standard.Option;
+import fr.vergne.parsing.layer.standard.Suite;
+
+/**
+ * A {@link SeparatedLoop} provides, rather than a sequence of adjacent
+ * {@link Element}s (e.g. AAAAA) like a classical {@link Loop}, a sequence of
+ * {@link Element}s is separated by {@link Separator}s (e.g. AXAXAXAXA).
+ * Consequently, the number of {@link Separator}s is always equal to the number
+ * of {@link Element}s - 1.
+ * 
+ * @author Matthieu Vergne <matthieu.vergne@gmail.com>
+ * 
+ * @param <Element>
+ * @param <Separator>
+ */
+public class SeparatedLoop<Element extends Layer, Separator extends Layer>
+		extends AbstractLayer {
+
+	private final Choice overall;
+	private final Option<Element> option;
+	private final Element head;
+	private final Loop<Suite> loop;
+
+	public SeparatedLoop(final Generator<Element> itemGenerator,
+			final Generator<Separator> separatorGenerator) {
+		head = itemGenerator.generates();
+		loop = new Loop<Suite>(new Generator<Suite>() {
+
+			@Override
+			public Suite generates() {
+				return new Suite(separatorGenerator.generates(),
+						itemGenerator.generates());
+			}
+		});
+		option = new Option<Element>(head);
+		overall = new Choice(option, new Suite(head, loop));
+	}
+
+	@Override
+	protected String buildRegex() {
+		return overall.getRegex();
+	}
+
+	@Override
+	protected void setInternalContent(String content) {
+		try {
+			overall.setContent(content);
+		} catch (ParsingException e) {
+			throw new ParsingException(this, overall, content, e.getStart(),
+					content.length(), e);
+		}
+	}
+
+	@Override
+	public String getContent() {
+		return overall.getContent();
+	}
+
+	public GreedyMode getMode() {
+		return loop.getMode();
+	}
+
+	public void setMode(GreedyMode mode) {
+		option.setMode(mode);
+		loop.setMode(mode);
+	}
+
+	/**
+	 * 
+	 * @return the number of {@link Element}s of this {@link SeparatedLoop}
+	 */
+	public int size() {
+		if (overall.getCurrent() instanceof Option) {
+			return option.isPresent() ? 1 : 0;
+		} else {
+			return 1 + loop.size();
+		}
+	}
+
+	/**
+	 * 
+	 * @param index
+	 *            the index of a parsed {@link Element}
+	 * @return the {@link Element} requested
+	 * @throws IndexOutOfBoundsException
+	 *             if the index relate to an inexistent {@link Element}
+	 */
+	@SuppressWarnings("unchecked")
+	public Element get(int index) throws IndexOutOfBoundsException {
+		if (overall.getCurrent() instanceof Option && option.isPresent()
+				&& index == 0) {
+			return option.getOption();
+		} else if (overall.getCurrent() instanceof Suite
+				&& loop.size() > index - 1 && index >= 0) {
+			return index == 0 ? head : (Element) loop.get(index - 1).get(1);
+		} else {
+			throw new IndexOutOfBoundsException("The index (" + index
+					+ ") should be between 0 and " + size());
+		}
+	}
+}
---
+++ parsing-core/src/test/java/fr/vergne/parsing/layer/util/SeparatedLoopTest.java
@@ -0,0 +1,179 @@
+package fr.vergne.parsing.layer.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fr.vergne.parsing.layer.exception.ParsingException;
+import fr.vergne.parsing.layer.standard.Atom;
+import fr.vergne.parsing.layer.standard.Loop.Generator;
+
+public class SeparatedLoopTest {
+
+	@Test
+	public void testSize() {
+		SeparatedLoop<IntNumber, Atom> separatedLoop = new SeparatedLoop<IntNumber, Atom>(
+				new Generator<IntNumber>() {
+
+					@Override
+					public IntNumber generates() {
+						return new IntNumber();
+					}
+				}, new Generator<Atom>() {
+
+					@Override
+					public Atom generates() {
+						return new Atom("-");
+					}
+				});
+
+		separatedLoop.setContent("");
+		assertEquals(0, separatedLoop.size());
+
+		separatedLoop.setContent("1");
+		assertEquals(1, separatedLoop.size());
+
+		separatedLoop.setContent("1-2");
+		assertEquals(2, separatedLoop.size());
+
+		separatedLoop.setContent("9-8-7");
+		assertEquals(3, separatedLoop.size());
+	}
+
+	@Test
+	public void testGetIndex() {
+		SeparatedLoop<IntNumber, Atom> separatedLoop = new SeparatedLoop<IntNumber, Atom>(
+				new Generator<IntNumber>() {
+
+					@Override
+					public IntNumber generates() {
+						return new IntNumber();
+					}
+				}, new Generator<Atom>() {
+
+					@Override
+					public Atom generates() {
+						return new Atom("-");
+					}
+				});
+
+		separatedLoop.setContent("1");
+		assertEquals(1, separatedLoop.get(0).getValue());
+
+		separatedLoop.setContent("1-2");
+		assertEquals(1, separatedLoop.get(0).getValue());
+		assertEquals(2, separatedLoop.get(1).getValue());
+
+		separatedLoop.setContent("9-8-7");
+		assertEquals(9, separatedLoop.get(0).getValue());
+		assertEquals(8, separatedLoop.get(1).getValue());
+		assertEquals(7, separatedLoop.get(2).getValue());
+	}
+
+	@Test
+	public void testGetWrongIndex() {
+		SeparatedLoop<IntNumber, Atom> separatedLoop = new SeparatedLoop<IntNumber, Atom>(
+				new Generator<IntNumber>() {
+
+					@Override
+					public IntNumber generates() {
+						return new IntNumber();
+					}
+				}, new Generator<Atom>() {
+
+					@Override
+					public Atom generates() {
+						return new Atom("-");
+					}
+				});
+
+		separatedLoop.setContent("");
+		try {
+			separatedLoop.get(-1);
+		} catch (IndexOutOfBoundsException e) {
+		}
+		try {
+			separatedLoop.get(0);
+		} catch (IndexOutOfBoundsException e) {
+		}
+
+		separatedLoop.setContent("1");
+		try {
+			separatedLoop.get(-1);
+		} catch (IndexOutOfBoundsException e) {
+		}
+		try {
+			separatedLoop.get(1);
+		} catch (IndexOutOfBoundsException e) {
+		}
+
+		separatedLoop.setContent("1-2");
+		try {
+			separatedLoop.get(-1);
+		} catch (IndexOutOfBoundsException e) {
+		}
+		try {
+			separatedLoop.get(2);
+		} catch (IndexOutOfBoundsException e) {
+		}
+
+		separatedLoop.setContent("9-8-7");
+		try {
+			separatedLoop.get(-1);
+		} catch (IndexOutOfBoundsException e) {
+		}
+		try {
+			separatedLoop.get(3);
+		} catch (IndexOutOfBoundsException e) {
+		}
+	}
+
+	@Test
+	public void testParsingException() {
+		SeparatedLoop<IntNumber, Atom> separatedLoop = new SeparatedLoop<IntNumber, Atom>(
+				new Generator<IntNumber>() {
+
+					@Override
+					public IntNumber generates() {
+						return new IntNumber();
+					}
+				}, new Generator<Atom>() {
+
+					@Override
+					public Atom generates() {
+						return new Atom("-");
+					}
+				});
+
+		try {
+			separatedLoop.setContent("-");
+			fail("No exception thrown.");
+		} catch (ParsingException e) {
+		}
+
+		try {
+			separatedLoop.setContent("-1");
+			fail("No exception thrown.");
+		} catch (ParsingException e) {
+		}
+
+		try {
+			separatedLoop.setContent("1-");
+			fail("No exception thrown.");
+		} catch (ParsingException e) {
+		}
+
+		try {
+			separatedLoop.setContent("-1-");
+			fail("No exception thrown.");
+		} catch (ParsingException e) {
+		}
+
+		try {
+			separatedLoop.setContent("1x2");
+			fail("No exception thrown.");
+		} catch (ParsingException e) {
+		}
+	}
+
+}
